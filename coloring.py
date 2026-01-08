f514df83eb747757db8d5b5cefb14558e1595366
N745160 <sallndiasse19@gmail.com>
2026-01-08 20:26:28 +0100

    Mise a jour
    Added functionality to specify coloring order in greedy_coloring.

--- coloring.py
+++ coloring.py
@@ -1,17 +1,26 @@
-from typing import Dict, Set
+from typing import Dict, Set, List
 
 Graph = Dict[str, Set[str]]
 
-def greedy_coloring(graph: Graph) -> Dict[str, int]:
+def greedy_coloring(graph: Graph, order: str = "largest_first") -> Dict[str, int]:
     """
-    Algorithme glouton de coloration.
-    Retourne un dict: sommet -> index de couleur.
+    Coloration gloutonne.
+    order:
+      - "as_is"         : ordre naturel du dict (moins bon)
+      - "largest_first" : trie les sommets par degré décroissant (souvent meilleur)
     """
-    color = {}
-    for node in graph:
+    if order == "largest_first":
+        nodes: List[str] = sorted(graph.keys(), key=lambda n: len(graph[n]), reverse=True)
+    else:
+        nodes = list(graph.keys())
+
+    color: Dict[str, int] = {}
+
+    for node in nodes:
         neighbor_colors = {color[n] for n in graph[node] if n in color}
         c = 0
         while c in neighbor_colors:
             c += 1
         color[node] = c
+
     return color
